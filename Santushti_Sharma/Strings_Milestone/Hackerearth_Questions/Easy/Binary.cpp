b7b216845a738eaa263a9e0954dff87eaa0481fc
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2020-06-06 17:57:44 +0530

    Create Binary.cpp

---
+++ Santushti_Sharma/Strings_Milestone/Hackerearth_Questions/Easy/Binary.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long int t;
+
+    cin>>t;
+    cin.ignore();
+
+    while(t--) {
+
+        int n;
+
+        cin>>n;
+
+        string s;
+
+        cin>>s;
+
+        multiset<long long int> a(s.begin(), s.end());
+
+        long long int cnt = a.count('1');
+
+        long long int ans = (cnt * (cnt -1))/2;
+
+        cout<<ans<<"\n";
+    }
+
+    return 0;
+}
