26fa79b264d9a04ccf2d0882993cf401e112451f
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2020-06-06 14:22:01 +0530

    Create Terrible_Chandu.cpp

---
+++ Santushti_Sharma/Strings_Milestone/Hackerearth_Questions/Easy/Terrible_Chandu.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    
+    cin>>t;
+        
+    while(t--) {
+        
+        string str;
+
+        cin>>str;
+         
+        int l = str.length();
+
+        for(int i = l- 1; i >= 0; --i) {
+            
+            cout<<str[i];
+        }
+        
+        cout<<"\n";
+    }
+        
+    return 0;
+}
