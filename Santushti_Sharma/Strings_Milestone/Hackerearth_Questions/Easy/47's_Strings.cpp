d80a0719f1ece3528d3100b9a99867b801f7a3dd
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2020-06-06 14:42:11 +0530

    Create 47's_Strings.cpp

---
+++ Santushti_Sharma/Strings_Milestone/Hackerearth_Questions/Easy/47's_Strings.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    long long int t;
+
+    cin>>t;
+
+    while(t--) {
+        
+        string s;
+
+        long long int ans = 0;
+
+        cin>>s;
+
+        int start = 0;
+
+        for(int i = 0; i < s.length(); ++i) {
+
+            if(s[i] == '$' || s[i] == '&') {
+
+                ans += (i +1 -start)*(s.length() -i);
+                start = i +1;
+            }
+        }
+
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
