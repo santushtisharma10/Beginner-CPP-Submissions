833ba5064e68a98ba59475e9e2a2277fd5304135
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2020-06-06 17:30:33 +0530

    Create Good_Subsequences.cpp

---
+++ Santushti_Sharma/Strings_Milestone/Hackerearth_Questions/Easy/Good_Subsequences.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    long long int t, m = 1000000007;
+
+    cin>>t;
+
+    while(t--) {
+
+        string s;
+
+        cin>>s;
+
+        long long int ans = 1;
+        
+        sort(s.begin(), s.end());
+
+        multiset<char> a(s.begin(), s.end());
+
+        for(int i = 1; i < s.length(); i += a.count(s[i])) {
+
+            ans *= (a.count(s[i]))%m;
+            ans %= m;
+        }
+        cout<<ans<<"\n";
+    }
+
+    return 0;
+}
