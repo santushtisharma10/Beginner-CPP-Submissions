44d4ec854fe44d4c1cbdb27bbaa1d3302c048fd1
Santushti Sharma <60578091+santushtisharma10@users.noreply.github.com>
2020-06-06 17:51:07 +0530

    Create Sorted_String.cpp

---
+++ Santushti_Sharma/Strings_Milestone/Hackerearth_Questions/Easy/Sorted_String.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+
+    cin>>t;
+
+    while(t--) {
+
+        string s;
+
+        cin>>s;
+
+        sort(s.begin(),s.end());
+
+        multiset<char> a(s.begin(),s.end());
+
+        vector<int> ans;
+        
+        unordered_map<int, string> mp;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            ans.push_back(a.count(s[i]));
+            mp[a.count(s[i])] += s[i];
+        }
+
+        sort(ans.begin(), ans.end());
+
+        multiset<int> cnt(ans.begin(),ans.end());
+
+        for(int i = 0; i < ans.size(); i += cnt.count(ans[i])) {
+
+            cout<<mp[ans[i]];
+        }
+        
+        cout<<"\n";
+    }
+    return 0;
+}
